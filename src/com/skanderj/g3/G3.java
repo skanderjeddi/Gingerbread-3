41eba2c09dca977c117340e0509276363163dc8f
= <skander.jeddi2@gmail.com>
2020-05-03 03:15:29 +0200

    First commit!

---
+++ src/com/skanderj/g3/G3.java
@@ -0,0 +1,9 @@
+package com.skanderj.g3;
+
+public final class G3 {
+	private static final String VERSION = "B.1";
+
+	public static void main(String[] args) {
+		System.out.printf("Gingerbread3 version %s - by SkanderJ\n", G3.VERSION);
+	}
+}
