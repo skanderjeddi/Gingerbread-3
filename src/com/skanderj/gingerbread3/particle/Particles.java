089410a08a5c6732e22c2ebbcef65dd93dfc0116
DaRealNim <darealnim@gmail.com>
2020-07-17 16:48:06 +0200

    oops Particles didn't import Moveable

--- src/com/skanderj/gingerbread3/particle/Particles.java
+++ src/com/skanderj/gingerbread3/particle/Particles.java
@@ -3,6 +3,7 @@ package com.skanderj.gingerbread3.particle;
 import com.skanderj.gingerbread3.core.Application;
 import com.skanderj.gingerbread3.core.Engine;
 import com.skanderj.gingerbread3.core.Priority;
+import com.skanderj.gingerbread3.core.Moveable;
 import com.skanderj.gingerbread3.core.object.ApplicationObject;
 import com.skanderj.gingerbread3.display.Screen;
 import com.skanderj.gingerbread3.logging.Logger;
